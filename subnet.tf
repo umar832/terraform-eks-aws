0d5c0bfb690686f7236917db4446ae6746207aa2
Umar <umaribadiullaev@gmail.com>
2021-09-30 18:08:14 +0600

    added vpc

---
+++ subnet.tf
@@ -0,0 +1,29 @@
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+} 
+
+resource "aws_subnet" "public3" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.2.0.0/16"
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.2.1.0/16"
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.2.2.0/16"
+}
